c4525367fbc123f4b98655eb8f8fc1f610e449a4
Nandhini S <nandhini18vgr@gmail.com>
2023-07-11 12:32:59 +0530

    simplified

--- src/PayRollProcessor.java
+++ src/PayRollProcessor.java
@@ -1,37 +1,15 @@
-import java.util.Scanner;
-abstract class Employee{
-    String name;
-    abstract double calculatePay();
-}
-class SalariedEmployee extends Employee{
-    double salary;
-    @Override
-    double calculatePay() {
+class Employee{
+    public double calculatePay(String name, double salary){
         return salary;
     }
-}
-class HourlyEmployee extends Employee{
-    double hourlyRate, hoursWorked;
-    @Override
-    double calculatePay() {
-        return hourlyRate*hoursWorked;
+    public double calculatePay(String name, double workingHours, double hourlyRate){
+        return workingHours*hourlyRate;
     }
 }
-public class PayRollProcessor {
+class PayRollProcessor{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        SalariedEmployee salariedEmployee = new SalariedEmployee();
-        HourlyEmployee hourlyEmployee = new HourlyEmployee();
-        System.out.print("Enter Employee name: ");
-        salariedEmployee.name = scanner.nextLine();
-        hourlyEmployee.name = salariedEmployee.name;
-        System.out.print("Enter salary: ");
-        salariedEmployee.salary  = scanner.nextDouble();
-        System.out.print("Enter Hourly rate: ");
-        hourlyEmployee.hourlyRate = scanner.nextDouble();
-        System.out.print("Enter Hours worked: ");
-        hourlyEmployee.hoursWorked = scanner.nextDouble();
-        System.out.println("Monthly salary: "+salariedEmployee.calculatePay());
-        System.out.println("Hourly salary: "+hourlyEmployee.calculatePay());
+        Employee lisa = new Employee();
+        System.out.println(lisa.calculatePay("Lisa", 8880.0));
+        System.out.println(lisa.calculatePay("Lisa", 45.5, 500.0));
     }
 }
